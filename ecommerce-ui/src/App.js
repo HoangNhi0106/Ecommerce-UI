41b2cea22ce64acac028cbe2298bfb6a26964d7b
HoangNhi0106 <47691938+HoangNhi0106@users.noreply.github.com>
2021-07-23 21:20:42 +0700

    fix

--- ecommerce-ui/src/App.js
+++ ecommerce-ui/src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
-import { ProductList } from './admin page/Products'
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import Product from "./components/product/Product";
+import Signup from "./components/signup/Signup";
+import Signin from "./components/signin/Signin";
+import Category from "./components/category/Category";
+import User from "./components/user/User";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import './App.css';
 
-const App = () => (
-    <Admin restClient={jsonServerRestClient('http://localhost:8080')}>
-        <Resource name="product" list={ProductList} />
-    </Admin>
-);
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="root">
+        <Navbar />
+
+        <Switch>
+          <Route exact path="/home">
+            <Home/>
+          </Route>
+          <Route exact path="/product">
+            <Product/>
+          </Route>
+          <Route exact path="/signup">
+            <Signup/>
+          </Route>
+          <Route exact path="/signin">
+            <Signin/>
+          </Route>
+          <Route exact path="/category">
+            <Category/>
+          </Route>
+          <Route exact path="/user">
+            <User/>
+          </Route>
+        </Switch>           
+      </div>
+    </BrowserRouter>
+  );
+}
 
 export default App;
